1f727fb8e0764d476f709e99d7f699911b711fe7
Jon Sharp <sharpjon@gmail.com>
2015-11-17 10:57:35 -0600

    Added help for padding output file

--- make_demo.sh
+++ make_demo.sh
@@ -2,3 +2,11 @@
 
 m68k-elf-gcc -o demo demo.s -nostdlib
 m68k-elf-objcopy -O binary demo floppy.img
+
+# optionally fill img to full floppy img size:
+
+# 400K:
+#dd if=/dev/zero of=floppy.img bs=1 count=0 seek=409600
+
+# 800K:
+#dd if=/dev/zero of=floppy.img bs=1 count=0 seek=819200
