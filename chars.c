0628c676b24136e742cf6a49f47383d3c2e1fd31
Jon Sharp <sharpjon@gmail.com>
2016-04-08 12:28:14 -0500

    New experimental branch:
     - Now loading code from floppy beyond initial boot block
     - Incorporating 5x13 font data
     - Calling C from ASM

---
+++ chars.c
@@ -0,0 +1,13 @@
+#include "chars.h"
+
+void doSomething() {
+	int x = 2;
+	int y = 8;
+	int z = x + y;
+	return;
+}
+
+int getChar(int charIndex) {
+	int foo[] = { 0, 0, 0 };
+	return 68;
+}
--- make_demo.sh
+++ make_demo.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -xe
 
-m68k-elf-gcc -o demo demo.s -nostdlib
+m68k-elf-gcc -Os -o demo demo.s chars.c -nostdlib -mno-rtd
+#m68k-elf-gcc -Os -o chars chars.c -nostdlib -nodefaultlibs
+#m68k-elf-objcopy --add-section .chars=chars demo
 m68k-elf-objcopy -O binary demo floppy.img
+#m68k-elf-objcopy -O binary chars chars.bin
 
 # optionally fill img to full floppy img size:
 
